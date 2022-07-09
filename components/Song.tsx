f5f31118eeab791bd5017c023608d6709e592612
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-09 10:38:19 +0100

    feat(song): truncate text

--- components/Song.tsx
+++ components/Song.tsx
@@ -26,18 +26,15 @@ export const Song = ({
 }: SongProps) => (
   <ClickableSong href={id}>
     <li
-      className={cn(
-        'grid grid-cols-4 items-baseline gap-2 overflow-scroll py-2.5',
-        {
-          'cursor-pointer hover:bg-stone-50 hover:pl-2 hover:font-bold':
-            !disabled,
-        }
-      )}
+      className={cn('grid grid-cols-4 items-baseline gap-2 py-2.5', {
+        'cursor-pointer hover:bg-stone-50 hover:pl-2 hover:font-bold':
+          !disabled,
+      })}
     >
-      <span className="col-span-1 text-sm uppercase text-stone-700">
+      <span className="col-span-1 truncate text-sm uppercase text-stone-700">
         {artist}
       </span>
-      <span className="col-span-2 text-sm uppercase text-stone-700">
+      <span className="col-span-2 truncate text-sm uppercase text-stone-700">
         {title}
       </span>
       <span
