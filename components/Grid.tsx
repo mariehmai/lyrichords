b9d091c95c78ea337ee603cf27b365a37cde1fc3
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-16 23:51:31 +0100

    refactor(app): remove unused React import

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 function Grid({ children }: React.PropsWithChildren<{}>) {
   return (
     <div className="flex w-full max-w-[800px] flex-col flex-wrap items-center justify-center md:w-auto md:flex-row">
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
