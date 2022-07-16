b39c2f00e4200ff1540a9748dcd95e3c2f06b9a1
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-16 19:09:30 +0100

    refactor(Navbar): use svg instead of img

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image';
-import logo from '../public/logo.svg';
+import Logo from './Logo';
 
 const Title = () => (
   <h1 className="font-light uppercase text-stone-700">Lyrichords</h1>
@@ -10,13 +9,7 @@ export const Navbar = () => (
   <nav className="fixed z-[100] -mx-4 flex h-[60px] w-full flex-row items-center bg-white bg-opacity-50 px-2 backdrop-blur-md">
     <Link href="/">
       <div className="flex cursor-pointer flex-row items-center gap-2">
-        <Image
-          loading="eager"
-          src={logo}
-          alt="lyrichords-logo"
-          width={48}
-          height={60}
-        />
+        <Logo />
         <Title />
       </div>
     </Link>
