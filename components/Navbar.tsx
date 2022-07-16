dffe271e0a27610efe07504ec06b25a5ed35b443
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-16 09:03:32 +0100

    feat(Navbar): load eagerly the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import logo from '../public/logo.svg';
 
 const Title = () => (
   <h1 className="font-light uppercase text-stone-700">Lyrichords</h1>
@@ -9,7 +10,13 @@ export const Navbar = () => (
   <nav className="fixed z-[100] -mx-4 flex h-[60px] w-full flex-row items-center bg-white bg-opacity-50 px-2 backdrop-blur-md">
     <Link href="/">
       <div className="flex cursor-pointer flex-row items-center gap-2">
-        <Image src="/logo.svg" alt="lyrichords-logo" width={48} height={60} />
+        <Image
+          loading="eager"
+          src={logo}
+          alt="lyrichords-logo"
+          width={48}
+          height={60}
+        />
         <Title />
       </div>
     </Link>
