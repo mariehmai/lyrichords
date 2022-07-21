baa97ba1d7fa19ecd2344a0475276d976b0a074f
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-21 19:17:25 +0100

    refactor(icon): update style of icon link and button

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -5,6 +5,8 @@ type IconButtonProps = {
   size?: Size;
   title?: string;
   label?: string;
+  withBorder?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
   Icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
 };
@@ -14,22 +16,29 @@ function IconButton({
   title = '',
   label = '',
   onClick = () => {},
+  withBorder = false,
+  disabled = false,
   Icon,
 }: IconButtonProps) {
   return (
     <button
-      className="group flex items-center gap-1 outline-red-400"
+      className={cn(
+        'group flex w-fit items-center gap-1 outline-red-400 active:shadow-inner',
+        {
+          'rounded-md border-2 border-stone-500 px-1.5 py-0.5 hover:shadow-sm':
+            withBorder,
+          'opacity-80': disabled,
+        }
+      )}
       title={title}
+      disabled={disabled}
       onClick={onClick}
     >
       <Icon
-        className={cn(
-          'cursor-pointer border-0 border-none text-stone-600 hover:opacity-70',
-          {
-            'h-5 w-5': size === 'sm',
-            'h-7 w-7': size === 'md',
-          }
-        )}
+        className={cn('cursor-pointer border-0 border-none text-stone-600', {
+          'h-5 w-5': size === 'sm',
+          'h-7 w-7': size === 'md',
+        })}
       />
       {!!label && (
         <span className={cn('text-sm text-stone-600 group-hover:underline')}>
--- components/IconLink.tsx
+++ components/IconLink.tsx
@@ -7,6 +7,7 @@ type IconButtonProps = {
   size?: Size;
   title?: string;
   label?: string;
+  withBorder?: boolean;
   Icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
 };
 
@@ -15,22 +16,23 @@ function IconButton({
   size = 'sm',
   title = '',
   label = '',
+  withBorder = false,
   Icon,
 }: IconButtonProps) {
   return (
     <Link href={href}>
       <a
-        className="group flex items-center gap-1 outline-red-400"
+        className={cn('group flex w-fit items-center gap-1 outline-red-400', {
+          'rounded-md border-2 border-stone-500 px-1.5 py-0.5 hover:shadow-sm':
+            withBorder,
+        })}
         title={title}
       >
         <Icon
-          className={cn(
-            'cursor-pointer border-0 border-none text-stone-600 hover:opacity-70',
-            {
-              'h-5 w-5': size === 'sm',
-              'h-7 w-7': size === 'md',
-            }
-          )}
+          className={cn('cursor-pointer border-0 border-none text-stone-600', {
+            'h-5 w-5': size === 'sm',
+            'h-7 w-7': size === 'md',
+          })}
         />
         {!!label && (
           <span className={cn('text-sm text-stone-600 group-hover:underline')}>
