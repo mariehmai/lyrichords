4fa146349772b219ed6631e1f7068acec97fe9a4
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-21 15:18:48 +0100

    refactor(app): fix layout of Grid and CardLink

--- components/CardLink.tsx
+++ components/CardLink.tsx
@@ -6,7 +6,7 @@ function CardLink({ href, children }: React.PropsWithChildren<CardProps>) {
   return (
     <a
       href={href}
-      className="m-4 max-w-[300px] rounded-lg border-2 border-stone-500 p-6 text-left text-inherit hover:border-red-400 hover:text-red-500"
+      className="w-full rounded-lg border-2 border-stone-500 p-6 text-left text-inherit hover:border-red-400 hover:text-red-500 md:max-w-[300px]"
     >
       {children}
     </a>
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,6 +1,6 @@
 function Grid({ children }: React.PropsWithChildren<{}>) {
   return (
-    <div className="flex w-full max-w-[800px] flex-col flex-wrap items-center justify-center md:w-auto md:flex-row">
+    <div className="flex w-full flex-col flex-wrap items-center justify-center gap-4 md:w-auto md:flex-row">
       {children}
     </div>
   );
